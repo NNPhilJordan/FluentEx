9479d620f97149cc55a6c2635a9a86bc0dd6657e
NNPhilJordan <23775@nelnet.net>
2021-06-23 16:47:56 -0600

    Initial Commit

---
+++ exampleClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentAssertions;
+
+namespace FluentEx
+{
+          public class exampleClass 
+          {
+            public void additionMethod()
+            {
+             var first = 3;
+             var second = 7;
+             var result = first + second;
+          
+ 
+             result.Should().Be(10);
+            }
+          }
+            
+        
+}    
